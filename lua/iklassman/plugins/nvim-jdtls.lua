e108ad2e0a00c00c346c256f470e1af5a31d5b72
Inacio Klassmann <inacioklassmann@gmail.com>
2024-04-05 09:50:52 -0300

    fixing jdtls runnable in different path

--- lua/iklassman/plugins/nvim-jdtls.lua
+++ lua/iklassman/plugins/nvim-jdtls.lua
@@ -1,30 +1,26 @@
+
+local function file_exists(name)
+   local f = io.open(name,"r")
+   if f ~= nil then io.close(f) return true else return false end
+end
+
+
 local home = os.getenv('HOME')
-local jdtls_dir = "/opt/homebrew/Cellar/jdtls/1.33.0/libexec"
 local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
 local workspace_dir = home .. "/.local/share/workspaces-jdtls/" .. project_name
+local jdtls_runnable = {"/usr/local/bin/jdtls", "/opt/homebrew/bin/jdtls"}
+local jdtls_cmd = jdtls_runnable[1]
+
+for i = 1, #jdtls_runnable do
+  if file_exists(jdtls_runnable[i]) then
+    jdtls_cmd = jdtls_runnable[i]
+    break
+  end
+end
 
 
 local config = {
-  -- cmd = {
-  --   home .. '/.sdkman/candidates/java/17.0.3.6.1-amzn/bin/java',
-  --   '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-  --   '-Dosgi.bundles.defaultStartLevel=4',
-  --   '-Declipse.product=org.eclipse.jdt.ls.core.product',
-  --   '-Dlog.protocol=true',
-  --   '-Dlog.level=ALL',
-  --   '-Xmx1g',
-  --   '--add-modules=ALL-SYSTEM',
-  --   '--add-opens', 'java.base/java.util=ALL-UNNAMED',
-  --   '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-  --   '-jar', jdtls_dir .. '/plugins/org.eclipse.equinox.launcher_1.6.700.v20231214-2017.jar',
-  --   '-configuration', jdtls_dir .. '/config_mac',
-  --   '-data', workspace_dir,
-  -- },
-  cmd = {
-    "/opt/homebrew/bin/jdtls",
-    "-data",
-    workspace_dir,
-  },
+  cmd = { jdtls_cmd, "-data", workspace_dir },
   root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw", "pom.xml" }, { upward = true })[1]),
 }
 local jdtls = require("jdtls")
