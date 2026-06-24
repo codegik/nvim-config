4ed29185ac55bdf0e7f3501863a8e57145a1d4ab
Inacio Klassmann <inacioklassmann@gmail.com>
2026-06-24 12:55:26 -0700

    adding keymap for claude-chat plugin

--- lua/plugins/claude-chat.lua
+++ lua/plugins/claude-chat.lua
@@ -1,10 +1,10 @@
 return {
   {
     "codegik/claude-chat.nvim",
-    cmd = { "ClaudeChat", "ClaudeChatReset", "ClaudeChatContinue", "ClaudeChatFile" },
+    cmd = { "ClaudeChat", "ClaudeChatReset", "ClaudeChatFile" },
+    dir = "/Users/iklassman/sources/codegik/claude-chat.nvim",
     keys = {
       { "<leader>ai", "<cmd>ClaudeChat<cr>", desc = "Claude Chat" },
-      { "<leader>ac", "<cmd>ClaudeChatContinue<cr>", desc = "Claude Chat Continue" },
       { "<leader>af", "<cmd>ClaudeChatFile<cr>", desc = "Claude Chat: add current file" },
     },
     config = function()
